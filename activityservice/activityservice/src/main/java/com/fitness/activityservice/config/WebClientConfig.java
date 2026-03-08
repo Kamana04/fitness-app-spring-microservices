0471df3125bbc6f4877d6e5f4de9bc6f440574a5
kamana1997 <104790201+kamana1997@users.noreply.github.com>
2026-03-08 18:03:45 +0530

    RabbitMQ setup and send activty data to RabbitMQ

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
@@ -16,7 +17,7 @@ public class WebClientConfig {
 
     @Bean
     public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder()
+        return webClientBuilder
                 .baseUrl("http://USER-SERVICE")
                 .build();
     }
--- activityservice/activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -5,17 +5,29 @@ import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.respository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
 
     private final UserValidationService userValidationService;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     public ActivityResponse trackActivity(ActivityRequest activityRequest) {
         boolean isValidUser = userValidationService.validateUser(activityRequest.getUserId());
@@ -32,6 +44,14 @@ public class ActivityService {
                 .build();
 
         Activity savedActivity = activityRepository.save(activity);
+
+        //publish to RabbitMQ for AI processing
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+
+        } catch (Exception e) {
+            log.error("Error sending message to RabbitMQ: {}", e.getMessage());
+        }
 
         return mapToActivityResponse(savedActivity);
     }
